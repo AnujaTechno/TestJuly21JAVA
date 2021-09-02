c9cfbdb67f95a4d5c0b33b7ef53426f321a1a9c2
Anuja Shrivastava <shrivastavaanuja1@gmail.com>
2021-09-02 23:25:17 +0530

    Uploading project

---
+++ src/p1/Test.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Test {
+	int x=10;
+	int y=20;
+	int z=x+y;
+	 public static void main(String[] args) {
+		 Test test=new Test();
+		 System.out.println(test.z);
+	}
+
+}
